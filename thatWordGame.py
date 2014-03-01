880216ac1a6e3958e6a53c2bd0ff2b6d0083682b
Nick Pierson <nkcrpn@gmail.com>
2014-02-28 21:07:34 -0600

    So progress. Much wow.

---
+++ thatWordGame.py
@@ -0,0 +1,18 @@
+#! /usr/bin/python
+
+import string
+
+userWord = raw_input("Choose a word: ")
+
+print
+
+alphabet = string.ascii_lowercase
+
+# in case the length of the alphabet changes
+for i in range(len(alphabet)):
+    letter = alphabet[i]
+
+    if letter no in userWord:
+        continue
+
+
